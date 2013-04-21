88192001363726f722523e0c29336066821582f4
Jonas Bernoulli <jonas@bernoul.li>
2013-04-21 16:56:40 +0200

    bump to 1.0.10

--- auto-compile.el
+++ auto-compile.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Created: 20080830
-;; Version: 1.0.9
+;; Version: 1.0.10
 ;; Status: beta
 ;; Package-Requires: ((cl-lib "0.2") (packed "0.3.3"))
 ;; Homepage: http://tarsius.github.com/auto-compile
