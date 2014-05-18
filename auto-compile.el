80f3c635969aa1fb06589fc7e5e88fb8946a235d
Jonas Bernoulli <jonas@bernoul.li>
2014-05-19 00:23:37 +0200

    improve library commentary
    Make it shorter and concentrate on the things users actually need to
    know.  Most importantly mention the new options that can be used to
    make automatic compilation less intrusive.

--- auto-compile.el
+++ auto-compile.el
@@ -29,80 +29,72 @@
 ;; Emacs Lisp source files.  Together these modes guarantee that Emacs
 ;; never loads outdated byte code files.
 
-;; `auto-compile-on-save-mode' recompiles source files when they are
+;; `auto-compile-on-save-mode' re-compiles source files when they are
 ;; being saved and `auto-compile-on-load-mode' does so before they are
-;; being loaded.  Both modes only _recompile_ a source file when the
-;; respective byte code file exists and is outdated.  Otherwise (when
-;; the byte code file doesn't exist or is up-to-date) these modes do
-;; _not_ compile the source file.
-
-;; To permanently or temporarily toggle automatic compilation of some
-;; source file use the command `toggle-auto-compile'.  Since the modes
-;; only ever _update_ byte code files, toggling automatic compilation
-;; is done simply by either creating the byte code file or removing
-;; it.  `toggle-auto-compile' can also toggle automatic compilation of
-;; multiple files at once; see it's doc-string for more information.
+;; being loaded (by advising `load' and `require').  Both modes only
+;; ever _re-compile_ a source file when the respective byte code file
+;; already exists but is outdated.  Otherwise they do _not_ compile
+;; the source file.
 
 ;; Even when using `auto-compile-on-save-mode' it can happen that some
-;; source file is newer than the respective byte code file.  This can
-;; for example happen when performing version control operations and
-;; is a problem because Emacs always loads the byte code file even
-;; when the respective source file has been modified since the former
-;; was created.
+;; source file is newer than the respective byte code file, which is a
+;; problem because by default Emacs load the byte code file even when
+;; the respective source file has been modified more recently.
 
-;; To prevent outdated byte code files from being loaded
-;; `auto-compile-on-load-mode' advises `require' and `load' to first
-;; recompile a source file if it is newer than the respective byte
-;; code file.
+;; Starting with Emacs version 24.4, setting `load-prefer-newer' to t
+;; prevents outdated byte code files from being loaded.  However this
+;; does not cause re-compilation of the source file, to actually do
+;; that `auto-compile-on-load-mode' is still required.
 
-;; Enabling `auto-compile-on-load-mode' as early as possible reduces
-;; the risk of loading an outdated byte code file.  It is best if you
-;; enable both modes together at the beginning of your init file, even
-;; before loading your package manager and having it initialize the
-;; `load-path'.
+;; Setup
+;; -----
 
-;;     ;; -*- no-byte-compile: t -*-
+;; To reduce the risk of loading outdated byte code files, enable
+;; `auto-compile-on-load-mode' as early as possible, preferably even
+;; before the package manager.  If your Emacs supports it, then also
+;; set `load-prefer-newer' to t even before requiring `auto-compile'.
+;; Then also enable `auto-compile-on-save-mode'.
+
+;;     ;;; init.el --- user init file  -*- no-byte-compile: t -*-
+;;     (add-to-list 'load-path "/path/to/packed")
 ;;     (add-to-list 'load-path "/path/to/auto-compile")
+;;     (setq load-prefer-newer t)
 ;;     (require 'auto-compile)
 ;;     (auto-compile-on-load-mode 1)
 ;;     (auto-compile-on-save-mode 1)
 
-;; Automatically compilation of Emacs Lisp source files is useful for
-;; at least the following reasons:
+;; Usage
+;; -----
 
-;; - Emacs prefers the byte code file over the source file even if the
-;;   former is outdated.  If you have to do the compilation manually
-;;   you will at least occasionally forget to do so and end up with an
-;;   old version of your code being loaded.
+;; Take note of the compile warnings and fix them.
 
-;; - There are many otherwise fine libraries to be found on the
-;;   Internet which when compiled will confront the user with a wall
-;;   of compile warnings and an occasional error.  If authors are
-;;   informed about these (often trivial) problems after each save
-;;   they will likely fix them quite quickly.  That or they have a
-;;   high noise tolerance.
+;; To permanently or temporarily toggle automatic compilation of some
+;; source file use the command `toggle-auto-compile'.  Since the modes
+;; only ever _update_ byte code files, toggling automatic compilation
+;; is done simply by either creating the byte code file or by removing
+;; it.  `toggle-auto-compile' can also toggle automatic compilation of
+;; multiple files at once; see its doc-string for more information.
 
-;; - It's often easier and less annoying to fix errors and warnings as
-;;   they are introduced than to do a "let's compile today's work and
-;;   see how it goes".
+;; Customization
+;; -------------
 
-;; This package is designed to stay out of your way as much as it can
-;; while still motivating you to get things fixed.  That might be
-;; annoying initially but less so once existing problems have been
-;; fixed.  By default Auto-Compile won't let you quit Emacs on the
-;; first attempt without fixing fatal errors in visited source files
-;; first.  To disable this set `auto-compile-mark-failed-modified' to
-;; nil.
+;; Constantly having the *Compile-Log* buffer pop up when a file is
+;; being saved can quickly become annoying.  Obviously the first thing
+;; you should do to about that is to actually fix outstanding issues.
 
-;; Also note that just because no warnings and/or errors are reported
-;; when a source file is compiled by the modes defined here this does
-;; not necessarily mean that users of your libraries won't see any.  A
-;; likely cause for this would be that you forgot to require a feature
-;; which is loaded on your system but not necessarily on the users'
-;; systems.  So you should still manually compile your packages before
-;; release:
-;;
-;;     emacs -batch -Q -L . -L ../dependency/ -f batch-byte-compile *.el
+;; Once you have done that you might also want to keep that buffer
+;; from being automatically displayed and instead only show the number
+;; of compile warnings for the current file in the mode-line.
+
+;;     (setq auto-compile-display-buffer nil)
+;;     (setq auto-compile-mode-line-counter t)
+
+;; To display the buffer use `M-x auto-compile-display-log' or click
+;; on the counter in the mode-line.
+
+;; Using `auto-compile-inhibit-compile-hook' it is possible to inhibit
+;; automatic compilation under certain circumstances; e.g. when HEAD
+;; is detached inside a Git repository (useful during rebase sessions).
 
 ;;; Code:
 
