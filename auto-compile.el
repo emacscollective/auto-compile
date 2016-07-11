1526e59ea8aaa1738c53b24673d62605dbbb5c96
Anders Bakken <agbakken@gmail.com>
2016-07-11 10:12:12 -0700

    Silently forgo compiling if the compiled file is not writable
    Re #15.

--- auto-compile.el
+++ auto-compile.el
@@ -762,6 +762,7 @@ file would get loaded."
           (when (setq el (packed-locate-library file nosuffix))
             (setq elc (byte-compile-dest-file el))
             (when (and (file-exists-p elc)
+                       (file-writable-p elc)
                        (file-newer-than-file-p el elc))
               (message "Recompiling %s..." el)
               (packed-byte-compile-file el)
