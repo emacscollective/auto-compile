f717469a63b72b1e3d820c98c0976e014a216534
Jonas Bernoulli <jonas@bernoul.li>
2022-11-30 13:57:51 +0100

    Resume development

--- auto-compile.el
+++ auto-compile.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/emacscollective/auto-compile
 ;; Keywords: compile convenience lisp
 
-;; Package-Version: 1.7.2
+;; Package-Version: 1.7.2.50-git
 ;; Package-Requires: ((emacs "25.1") (compat "28.1.1.0") (packed "3.0.3"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
