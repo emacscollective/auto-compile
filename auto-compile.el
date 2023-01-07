c6a3b9f1ffd8ce057297f7197914b7e80fd3b218
Jonas Bernoulli <jonas@bernoul.li>
2023-01-07 16:09:30 +0100

    Bump copyright years

--- auto-compile.el
+++ auto-compile.el
@@ -1,6 +1,6 @@
 ;;; auto-compile.el --- Automatically compile Emacs Lisp libraries  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2008-2022 Jonas Bernoulli
+;; Copyright (C) 2008-2023 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/emacscollective/auto-compile
