emacscollective/auto-compile

commit e622c89780d6922d4d3cb9061ee726b7e1222eb8

Remove support for updating loaddefs
In Emacs 29 "loaddefs-gen.el" replaces "autoload.el", and while the
old implementation supported updating autoloads for a single file, the
new one provides to replacement for `autoload-generate-file-autoloads'.

auto-compile.el
@@ -245,14 +245,6 @@ is made to compile the file as that would obviously fail also."
   :group 'auto-compile
   :type 'boolean)
 
-(defcustom auto-compile-update-autoloads nil
-  "Whether to update autoloads after compiling.
-
-If no autoload file as specified by `packed-loaddefs-file' can be
-found quietly skip this step."
-  :group 'auto-compile
-  :type 'boolean)
-
 (defcustom auto-compile-inhibit-compile-hook nil
   "Hook used to inhibit automatic compilation.
 
@@ -524,7 +516,7 @@ pretend the byte code file exists.")
   (when (run-hook-with-args-until-success 'auto-compile-inhibit-compile-hook)
     (cl-return-from auto-compile-byte-compile))
   (let ((default-directory default-directory)
-        dest buf auto-compile-file-buffer success loaddefs)
+        dest buf auto-compile-file-buffer success)
     (when (and file
                (setq buf (get-file-buffer file))
                (buffer-modified-p buf)
@@ -578,27 +570,14 @@ pretend the byte code file exists.")
            (message "Byte-compiling %s failed" file)
            (auto-compile-handle-compile-error file buf start)
            (setq success nil)))
-        (when (and auto-compile-update-autoloads
-                   (setq loaddefs (packed-loaddefs-file)))
-          (with-suppressed-warnings ((obsolete autoload))
-            (require 'autoload))
-          (condition-case nil
-              (packed-with-loaddefs loaddefs
-                (let ((autoload-modified-buffers nil))
-                  (autoload-generate-file-autoloads
-                   file nil generated-autoload-file)))
-            (error
-             (message "Generating loaddefs for %s failed" file)
-             (setq loaddefs nil))))
         (pcase success
           ('no-byte-compile)
-          ('t (message "Wrote %s.{%s,%s}%s"
+          ('t (message "Wrote %s.{%s,%s}"
                        (file-name-sans-extension
                         (file-name-sans-extension file))
                        (progn (string-match "\\(\\.[^./]+\\)+$" file)
                               (substring (match-string 0 file) 1))
-                       (file-name-extension dest)
-                       (if loaddefs " (+)" "")))
+                       (file-name-extension dest)))
           (_  (message "Wrote %s (byte-compiling failed)" file))))
       success)))
 

commit 35eaeb3534965b663a0160237c51943ad10bc3ad

Remove declarations obsoleted by autoload functionality removal
Fixes: e622c89780d6 ("Remove support for updating loaddefs")

auto-compile.el
@@ -131,12 +131,6 @@
 
 (eval-when-compile (require 'subr-x))
 
-(declare-function autoload-rubric "autoload")
-(declare-function autoload-find-destination "autoload")
-(declare-function autoload-file-load-name "autoload")
-(declare-function autoload-generate-file-autoloads "autoload")
-
-(defvar autoload-modified-buffers)
 (defvar warning-minimum-level)
 
 (defvar auto-compile-use-mode-line)