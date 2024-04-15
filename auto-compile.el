01844064e3f2bb9f109a8a064998baf89a864fbc
Jonas Bernoulli <jonas@bernoul.li>
2024-04-15 17:33:25 +0200

    Update contact information

--- auto-compile.el
+++ auto-compile.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2008-2024 Jonas Bernoulli
 
-;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <emacs.auto-compile@jonas.bernoulli.dev>
 ;; Homepage: https://github.com/emacscollective/auto-compile
 ;; Keywords: compile convenience lisp
 
