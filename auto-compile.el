d0776bc6b35be111c7baafacb71cafb42ba3ca87
Jonas Bernoulli <jonas@bernoul.li>
2024-06-23 11:26:11 +0200

    Only use global-font-lock-mode-enable-in-buffer if available
    Since [1: 9506b9392e8], Emacs 30's `define-globalized-minor-mode'
    defines and relies on this function.  The issue that made that
    change necessary presumably doesn't exist on older Emacs releases,
    or at least I cannot remember, running into the issue before, which
    lead to [2: 4a951af], which fixed the issue for Emacs 30, but caused
    a void-function error on older releases.
    
    Closes #36.
    
    1: 2024-04-08 9506b9392e8c3548b769d9951a1c9d4abad9ee21
       (define-globalized-minor-mode): Fix bug#69431
    
    2: 2024-06-08 4a951af6b186ce86d324460d6629bc0ed391f482
       auto-compile--byte-compile-file: Restore font-lock activation

--- auto-compile.el
+++ auto-compile.el
@@ -630,7 +630,8 @@ pretend the byte code file exists.")
 
 (defun auto-compile--byte-compile-file (file)
   (let ((after-change-major-mode-hook
-         (list 'global-font-lock-mode-enable-in-buffer))
+         (and (fboundp 'global-font-lock-mode-enable-in-buffer)
+              (list 'global-font-lock-mode-enable-in-buffer)))
         (prog-mode-hook nil)
         (emacs-lisp-mode-hook nil))
     (byte-compile-file file)))
