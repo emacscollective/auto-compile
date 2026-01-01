eaed414fa7dae04c7701dbc591de52fb3e81aa91
Jonas Bernoulli <jonas@bernoul.li>
2026-01-01 19:21:56 +0100

    Release version 2.1.2

--- auto-compile.el
+++ auto-compile.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/emacscollective/auto-compile
 ;; Keywords: compile convenience lisp
 
-;; Package-Version: 2.1.1
+;; Package-Version: 2.1.2
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
